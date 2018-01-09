41ee1c9c44615345ed449f9d1584178b6fbca32e
33Yura33 <pomarenko1998@gmail.com>
2018-01-09 15:12:24 +0200

    work file

--- js/script.js
+++ js/script.js
@@ -1,31 +1,44 @@
 	///////1
 	document.getElementById('coordinats').onmousemove = function(e) {
-	  var x = e.offsetX==undefined?e.layerX:e.offsetX;
-	  var y = e.offsetY==undefined?e.layerY:e.offsetY;
-	 $("#pcoordinats").text(x+":"+y);
+	  var x = e.offsetX === undefined ? e.layerX : e.offsetX;
+	  var y = e.offsetY === undefined ? e.layerY : e.offsetY;
+	  $("#pcoordinats").text(x+":"+y);
 	}
 	///////2
 	document.getElementById('divimg').ondblclick = function(e) {
 		$('#divimg').css('background-image', 'url(http://s1.iconbird.com/ico/0512/Pleasant/file1337790523.png)');
 	}
 	////////3
-	var father =document.getElementById('fatherdivs');
-	
+
+
 	window.onload=function(){
+
+		var father = document.createElement('div');
+		father.id = "fatherdivs";
+
+		var divs = [];
+
 		for (var i =0; i <300; i++) {
+
 			var d=document.createElement('div');
 			d.style.width='30px';
 			d.style.height='30px';
 			d.style.background='gray';
 			d.className="divs";
 			d.id="divs";
+
 			father.appendChild(d);
+
 		}
 
+		document.getElementById("divimg").parentNode.insertBefore(father, document.getElementById("divimg").nextSibling);
+
 	}
+
 	$('body').on('click', '.divs', function(){
 		$(this).css("border-radius", "15px");
 	});
+
 	//////4
 	$('body').on('click', '.setimg', function(){
 			var put = $(this).attr('src');
@@ -40,24 +53,13 @@
 		 $(this).offset({top:offset.top+100});
 	});
 	//6
+
+	var state = 0;
+
 	$(".trigger div").click(function(e) {
-		  var class_ = $(this).attr('class');
-		  if(class_ == 'switch1')
-		  {
-		  	$(this).removeClass('switch1');
-		  	$(this).addClass('switch2');
-		  	console.log("1");
-		  }
-		  else if(class_ == 'switch2')
-		  {
-		  	$(this).removeClass('switch2');
-		  	$(this).addClass('switch3');
-		  	console.log("2");
-		  }
-		  else if(class_ == 'switch3')
-		  {
-		  	$(this).removeClass('switch3');
-		  	$(this).addClass('switch1');
-		  	console.log("0");
-		  }
+
+		state = (state == 2) ? 0 : ++state;
+
+		$(this).css({ marginLeft: state * 35 });
+
 	});
